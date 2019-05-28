66092a59038fc718d5d99e3e709817987284d824
Jeremy Adam <underway113@gmail.com>
2019-05-28 17:15:21 +0700

    Add Video Capture + ML

--- SeeMLVideo/ViewController.swift
+++ SeeMLVideo/ViewController.swift
@@ -7,12 +7,51 @@
 //
 
 import UIKit
+import AVKit
+import Vision
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        let captureSession = AVCaptureSession()
+        captureSession.sessionPreset = .photo
+        
+        guard let captureDevice = AVCaptureDevice.default(for: .video) else {return}
+        guard let input = try? AVCaptureDeviceInput(device: captureDevice) else {return}
+        captureSession.addInput(input)
+
+        captureSession.startRunning()
+        
+        //Capture Sample Layer Only for Detect
+        let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        view.layer.addSublayer(previewLayer)
+        previewLayer.frame = view.frame
+        
+        //Output
+        let dataOutput = AVCaptureVideoDataOutput()
+        dataOutput.setSampleBufferDelegate(self, queue: DispatchQueue(label: "videoQu"))
+        captureSession.addOutput(dataOutput)
+        
+        
+    }
+    
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        guard let pixelBuff: CVPixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else {return}
+        
+        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else {return}
+        let request = VNCoreMLRequest(model: model) { (req, err) in
+            
+            guard let result = req.results as? [VNClassificationObservation] else {return}
+            guard let firstRes = result.first else {return}
+            
+            print(firstRes.identifier, firstRes.confidence)
+            
+        }
+        
+        try? VNImageRequestHandler(cvPixelBuffer: pixelBuff, options: [:]).perform([request])
     }
 
 
